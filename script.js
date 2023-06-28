3db9f532eba508a020619f91a5a2bf6115dc7b8a
Avile Sindelo <avilesindelo@gmail.com>
2023-06-28 16:21:07 +0200

    seventh commit

--- script.js
+++ script.js
@@ -17,6 +17,30 @@ hiddenElements.forEach((elem) => {
 });
 
 
-const progressElems = document.querySelectorAll('.progress');
-progressElems.forEach((bar) => console.log(bar.innerHTML));
-console.log(progressElems);
+//Send email
+const submitBtn = document.querySelector('#submitBtn');
+const fullName = document.querySelector('#name-input');
+const emailID = document.querySelector('#email-input');
+const emailMessage = document.querySelector('#contact-message');
+
+submitBtn.addEventListener('click', ()=>{
+    if(fullName.value == ''){
+        alert('Please fill in your name');
+    } else if(emailID.value == ''){
+        alert('Please enter your email address')
+    } else if(emailMessage.value == ''){
+        alert('Please include a message in your email for the developer to read.')
+    } else { //All fields filled successfully
+        var params = {
+            from_name: fullName.value,
+            email_id: emailID.value,
+            message: emailMessage.value
+        }
+
+        emailjs.send("service_0uoqn1h", "template_cfqu6aa", params)
+                .then(function(res){
+                    alert('Success!!' + res.status);
+                });
+    }
+
+});
